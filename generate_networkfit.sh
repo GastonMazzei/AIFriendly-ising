512c87dd20f0e6a8a9261d8d92c48565d78b2e6a
Gaston Mazzei <gastonmazzei95@gmail.com>
2020-10-28 14:59:03 -0300

    final-version

--- generate_networkfit.sh
+++ generate_networkfit.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 echo "INFO: About to train a neural network. Classification of the Magnetization for the threshold 0.5 with a bi-layer with 3 neurons per layer is the default behaviour. \n Running 'python3 scripts/network_class.py NEURONS EPOCHS THRESHOLD' is an alternative for custom conditions\ne.g. 'python3 scripts/network_class.py 5 120 0.7'"
-python3 scripts/network_class.py 3 70 0.5
+for i in {1,2,3,4}
+do
+  python3 scripts/network_class.py 3 70 $i 0.5
+done
 echo "SUCCESS!"
--- scripts/network_class.py
+++ scripts/network_class.py
@@ -16,43 +16,47 @@ from keras.models import Sequential
 from keras.losses import binary_crossentropy, mean_squared_error
 from keras.optimizers import SGD
 
-from sklearn.metrics import roc_curve
+from sklearn.metrics import roc_curve, roc_auc_score as ras
 from sklearn.preprocessing import MinMaxScaler, StandardScaler, KBinsDiscretizer, PowerTransformer, Binarizer
 
 
-# In[19]:
+def load(name, what_to_fit, absolute=True):
+    out = pd.read_csv(name,low_memory=False).dropna().sample(frac=1)[['field','T',what_to_fit]]
+    if absolute: out[what_to_fit]=out[what_to_fit].apply(abs)
+    print(out.head())
+    return out
 
-
-def load(name):
-    return pd.read_csv(name,low_memory=False).dropna().sample(frac=1)
-
-
-def preprocess(df, threshold):
-    #
+def preprocess(df, threshold,):
     data = {}
-    print(df.head())
-    df['M'] = df['M'].apply(abs)
     x, y = df.to_numpy()[:,:-1].reshape(-1,len(df.columns)-1), df.to_numpy()[:,-1].reshape(-1,1)
-    print(f'SHAPE OF X IS ',x.shape)
+    def split_according_to_threshold(y,threshold):
+      effective_threshold=threshold
+      L=len(y)
+      result = sum(Binarizer(threshold= effective_threshold).fit_transform(y.reshape(-1,1)))/L
+      tol = 0.01
+      if not (threshold - tol <  result < threshold + tol):
+        protect = 0
+        while (not (threshold - tol <  result < threshold + tol)) and protect<100:
+          if threshold-tol<result:
+            effective_threshold += 0.01
+          else:
+            effective_threshold -= 0.01
+          result = sum(Binarizer(threshold= effective_threshold).fit_transform(y.reshape(-1,1)))/L
+          protect += 1
+      return Binarizer(threshold= effective_threshold).fit_transform(y.reshape(-1,1))
+
     if True:
       for s in [StandardScaler]:
         x=s().fit_transform(x)
-      y = Binarizer(threshold=threshold).fit_transform(y)    
+      y = split_according_to_threshold(y,threshold)    
     L = df.shape[0]
     divider = {'train':slice(0,int(0.7*L)),
                'val':slice(int(0.7*L),int((0.7+0.15)*L)),
                'test':slice(-int(0.15*L),None),}
-    #
     for k,i in divider.items():
         data[k] = (x[i],y[i])
-        print(f'for key {k} {np.count_nonzero(data[k][1])/len(data[k][1])*100}% are non-zero')
-        print(f'{data[k][0].shape}')
-    #
-    if False:
-      answ = input('if you are happy with the ratio, press "y"... else "n"')
-      if answ=='y': return data
-      else: sys.exit(1)
-    else: return data
+        print(f'for key {k} the fractions of positives is{np.count_nonzero(data[k][1])/len(data[k][1])*100}%')
+    return data
         
 def manage_database(name):
   with open(name,'rb') as f:
@@ -79,18 +83,6 @@ def create_and_predict(data,**kwargs):
             Dense(
                 kwargs.get('neurons',32),
                 activation=act,),
-            #Dense(
-            #    kwargs.get('neurons',32),
-            #    activation=act,),
-            #Dense(
-            #    kwargs.get('neurons',32),
-            #    activation=act,),
-            #Dense(
-            #    kwargs.get('neurons',32),
-            #    activation=act,),
-            #Dense(
-            #    kwargs.get('neurons',32),
-            #    activation=act,),
             Dense(
                 1,
                 activation='sigmoid'),
@@ -118,58 +110,52 @@ def create_and_predict(data,**kwargs):
     results['specs'] = kwargs
     #
     if kwargs.get('plot',False):
-        regression = False
-        case = 'val'
-        f, ax = plt.subplots(1,3)
-        if not regression:
-          fpr, tpr, treshold = roc_curve(
-                results['ytrue_'+case], results['ypred_'+case]
-                    )
-          ax[0].plot(fpr, tpr)
-        
-          weights = {0:[],1:[]}
-          for i,x in enumerate(results['ypred_'+case]):
-            weights[data[case][1][i][0]] += [x[0]]
-
-          #sns.distplot(data=pd.DataFrame(weights),ax=ax[1],kde=False)
-       
-          ax[1].hist(weights[0],label='0',alpha=0.5)
-          ax[1].hist(weights[1],label='1',alpha=1)
-          ax[1].set_xlim(0,1)
-          ax[1].legend()
-
-        else:
-          from scipy.optimize import curve_fit
-          def f(x,a): return a*x**(3/2)
-          #p, _ = curve_fit(f,data[case][0].flatten(),results['ypred_'+case].flatten())
-          p, _ = curve_fit(f,data[case][0].flatten(),data[case][1].flatten())
-          print(f'\n\n\n\n{p}\n\n\n\n')
-          new_x = np.linspace(min(data[case][0]),max(data[case][0]),1000)
-          ax[1].scatter(data[case][0],data[case][1],label='true values '+case,c='r')
-          ax[1].scatter(data[case][0],results['ypred_'+case],label='predicted values '+case,c='b')
-          ax[1].scatter(new_x,[f(q,p[0]) for q in new_x],label=r'$T \propto R^{\frac{3}{2}}$',c='g',alpha=0.5) 
-          ax[1].set_xlabel('$\propto$ T')
-          ax[1].set_ylabel('$\propto$ Radius')
-          ax[1].legend()
+        case = 'test'
+        from sklearn.linear_model import LogisticRegression as lr
+        f, ax = plt.subplots(1,3, figsize=(20,7))
+        fpr, tpr, treshold = roc_curve(
+              results['ytrue_'+case], results['ypred_'+case]
+                  )
+        ax[0].plot(tuple(fpr), tuple(tpr), label = 'NN AUC '+ str(
+                                            round(
+                                         ras(results['ytrue_'+case], results['ypred_'+case]),2)))
+        newytrue, newypred = data[case][1], lr(max_iter=5000).fit(*data['train']).predict_proba(data[case][0])[:,1]
+        fpr, tpr, treshold = roc_curve(
+              newytrue, newypred
+                  )
+        ax[0].plot(tuple(fpr), tuple(tpr), label = 'Logistic AUC '+ str(
+                                            round(
+                                         ras(newytrue, newypred), 2)))
+        ax[0].set_title('ROC curve')
+        ax[0].legend()
+        weights = {0:[],1:[]}
+        for i,x in enumerate(results['ypred_'+case]):
+          weights[data[case][1][i][0]] += [x[0]]
+     
+        ax[1].hist(weights[0],label='0',alpha=0.5)
+        ax[1].hist(weights[1],label='1',alpha=0.5)
+        ax[1].set_xlim(0,1)
+        ax[1].set_title('Output Weights per category')
+        ax[1].legend()
 
         ax[2].plot(results['accuracy'],c='b',label='train')
         ax[2].plot(results['val_accuracy'],c='g')
         ax[2].plot(results['loss'],c='b')
         ax[2].plot(results['val_loss'],c='g',label='validation')
         ax[2].legend()
+        ax[2].set_title('Training & Validation accuracy & loss')
         ax[2].set_ylim(0,1)
-        plt.show()
-        if False:
-            plt.plot(
-                *roc_curve(
-                    results['ytrue_test'], results['ypred_test']
-                        )[:-1])
+        plt.savefig(f'gallery/network-result-{kwargs["what_to_fit"]}')
     return results
 
 if __name__=='__main__':
-    # DEFAULT NEURONS SHOULD BE 2 JEJEJE
-    import sys                                            # threshold
-    create_and_predict(preprocess(load('dataset/miniising.csv'), float(sys.argv[3])),
-            neurons=int(sys.argv[1]), epochs=int(sys.argv[2]),plot=True)
+    # --argv 1-- DEFAULT NEURONS SHOULD BE 2
+    # --argv 2-- DEFAULT EPOCHS COULD BE 50 
+    # --argv 3-- WHAT TO FIT CAN BE 1,2,3,4: 'M','X','C','E' respectively...
+    # --argv 4-- THRESHOLD IS WHAT FRACTION OF POSITIVES WE TRAIN WITH
+    what_to_fit_dictionary = {'1':'M','2':'X','3':'C','4':'E'}
+    import sys                          
+    create_and_predict(preprocess(load('dataset/ising.csv',what_to_fit_dictionary[sys.argv[3]]), float(sys.argv[4])),
+            neurons=int(sys.argv[1]), epochs=int(sys.argv[2]), plot=True, what_to_fit=what_to_fit_dictionary[sys.argv[3]])
 
 
